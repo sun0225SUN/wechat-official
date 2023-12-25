770a661eb00500415b727c932370e4f454b1f3f5
guoqi.sun <sun412926@gmail.com>
2023-12-25 11:05:04 +0800

    :bug: fix: use vite

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-w-[375px] h-[100vh] flex justify-center items-center bg-[#f6f7f8]">
+    <div class="p-8 rounded-[16px] bg-white">
+      <div class="flex justify-center items-center">
+        <img class="w-[256px] h-[256px]" src="https://files.sunguoqi.com/images/202312222234058.webp">
+      </div>
+      <div class="mt-2 my-4 text-[13px] text-center text-gray-500">扫描二维码关注公众号</div>
+      <div class="w-full flex items-center py-3 rounded-[10px] bg-[#f7f7f7]">
+        <img class="w-[48px] h-[48px] mx-4 rounded-full" src="https://files.sunguoqi.com/images/202312222234584.webp">
+        <div class="flex flex-col justify-between">
+          <div class="font-bold">小孙同学 Coding</div>
+          <div class="text-[12px] text-gray-500">热爱可抵岁月漫长</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
